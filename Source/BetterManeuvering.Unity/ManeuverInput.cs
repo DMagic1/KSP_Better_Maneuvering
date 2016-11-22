55ce4c4b12e6517d56b6bbd15ae12f04a6a3b40e
DMagic <david.grandy@gmail.com>
2016-11-22 17:45:58 -0500

    Move DeltaV increment button function to upstream assembly

--- Source/BetterManeuvering.Unity/ManeuverInput.cs
+++ Source/BetterManeuvering.Unity/ManeuverInput.cs
@@ -8,6 +8,7 @@ namespace BetterManeuvering.Unity
 {
 	public class ManeuverInput : MonoBehaviour, IBeginDragHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 	{
+		public Toggle WindowToggle = null;
 		public Button ProgradeDownButton = null;
 		public Button ProgradeUpButton = null;
 		public Button NormalDownButton = null;
@@ -15,6 +16,12 @@ namespace BetterManeuvering.Unity
 		public Button RadialDownButton = null;
 		public Button RadialUpButton = null;
 		public Button ResetButton = null;
+		public Button ProgradeIncDownButton = null;
+		public Button ProgradeIncUpButton = null;
+		public Button NormalIncDownButton = null;
+		public Button NormalIncUpButton = null;
+		public Button RadialIncDownButton = null;
+		public Button RadialIncUpButton = null;
 		public TextHandler ProgradeText = null;
 		public TextHandler NormalText = null;
 		public TextHandler RadialText = null;
@@ -24,10 +31,6 @@ namespace BetterManeuvering.Unity
 		public TextHandler ResetText = null;
 		public TextHandler TotaldVText = null;
 		public Transform TitleTransform = null;
-
-		public float prograde_increment = 0.1f;
-		public float normal_increment = 0.1f;
-		public float radial_increment = 0.1f;
 
 		private RectTransform rect;
 		private Vector2 mouseStart;
@@ -48,126 +51,6 @@ namespace BetterManeuvering.Unity
 		{
 			gameObject.SetActive(false);
 			Destroy(gameObject);
-		}
-
-		public void ProIncrementDown()
-		{
-			prograde_increment /= 10;
-
-			if (prograde_increment < 0.01f)
-				prograde_increment = 0.01f;
-
-			if (ProIncrementText != null)
-			{
-				string units = "F0";
-
-				if (prograde_increment < 0.09f)
-					units = "F2";
-				else if (prograde_increment < 0.9f)
-					units = "F1";
-
-				ProIncrementText.OnTextUpdate.Invoke(prograde_increment.ToString(units));
-			}
-		}
-
-		public void ProIncrementUp()
-		{
-			prograde_increment *= 10;
-
-			if (prograde_increment > 100f)
-				prograde_increment = 100f;
-
-			if (NormIncrementText != null)
-			{
-				string units = "F0";
-
-				if (prograde_increment < 0.09f)
-					units = "F2";
-				else if (prograde_increment < 0.9f)
-					units = "F1";
-
-				ProIncrementText.OnTextUpdate.Invoke(prograde_increment.ToString(units));
-			}
-		}
-
-		public void NormIncrementDown()
-		{
-			normal_increment /= 10;
-
-			if (normal_increment < 0.01f)
-				normal_increment = 0.01f;
-
-			if (ProIncrementText != null)
-			{
-				string units = "F0";
-
-				if (normal_increment < 0.09f)
-					units = "F2";
-				else if (normal_increment < 0.9f)
-					units = "F1";
-
-				NormIncrementText.OnTextUpdate.Invoke(normal_increment.ToString(units));
-			}
-		}
-
-		public void NormIncrementUp()
-		{
-			normal_increment *= 10;
-
-			if (normal_increment > 100f)
-				normal_increment = 100f;
-
-			if (NormIncrementText != null)
-			{
-				string units = "F0";
-
-				if (normal_increment < 0.09f)
-					units = "F2";
-				else if (normal_increment < 0.9f)
-					units = "F1";
-
-				NormIncrementText.OnTextUpdate.Invoke(normal_increment.ToString(units));
-			}
-		}
-
-		public void RadIncrementDown()
-		{
-			radial_increment /= 10;
-
-			if (radial_increment < 0.01f)
-				radial_increment = 0.01f;
-
-			if (RadIncrementText != null)
-			{
-				string units = "F0";
-
-				if (radial_increment < 0.09f)
-					units = "F2";
-				else if (radial_increment < 0.9f)
-					units = "F1";
-
-				RadIncrementText.OnTextUpdate.Invoke(radial_increment.ToString(units));
-			}
-		}
-
-		public void RadIncrementUp()
-		{
-			radial_increment *= 10;
-
-			if (radial_increment > 100f)
-				radial_increment = 100f;
-
-			if (RadIncrementText != null)
-			{
-				string units = "F0";
-
-				if (radial_increment < 0.09f)
-					units = "F2";
-				else if (radial_increment < 0.9f)
-					units = "F1";
-
-				RadIncrementText.OnTextUpdate.Invoke(radial_increment.ToString(units));
-			}
 		}
 
 		public void OnBeginDrag(PointerEventData eventData)
